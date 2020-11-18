3b949b52e55eee821cdf5c798639a04e81fd04d8
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-11-18 15:42:34 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -35,7 +35,7 @@ end
 # However, this time, sort each internal array alphabetically by the first character
 
 def matrix_lookup(matrix, row, column)
-  ORGANIC_PRODUCE[matrix][row][column], ORGANIC_PRODUCE[matrix][row][column]
+  ORGANIC_PRODUCE[matrix][row][column] CONVENTIONAL_PRODUCE[matrix][row][column]
 end
 
 def matrix_update(matrix, row, column, new_value)
